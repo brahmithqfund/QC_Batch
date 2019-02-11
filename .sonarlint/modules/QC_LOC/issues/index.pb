e6ec1484147d95a3312426d02e48f851dd02c535
P Brahmith <brahmithchary.p@qfund@.net>
2019-02-11 16:20:14 +0530

    first commit on 11-02-2019 by brahmith

---
+++ .sonarlint/modules/QC_LOC/issues/index.pb
@@ -0,0 +1,9 @@
+
+V
+&src/test/java/tests/Objects.properties,8\6\867f41f2355d714b16278d986529251e56c6cefd
+P
+ src/test/java/tests/QCStore.java,f\1\f1f5f4816296e60d7232d0a00b509d48935fb3a4
+W
+'src/test/java/tests/VObjects.properties,d\3\d30bfd7e1bd7dd48e8d6863a9e9fc5ee33d94b8c
+Z
+*src/main/java/tests/VQCCSRLoginLogout.java,e\6\e6b68d01758e174cfbbfe4228166ab114abfff8e
---
+++ QC_TLP/src/main/java/tests/JQCDrawerAssign.java
@@ -0,0 +1,104 @@
+package tests;
+
+
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+public class JQCDrawerAssign extends QCStore{
+	public static void drawerAssign(String SSN,String AppURL)
+	{
+		
+		try{
+				int lastrow=TestData.getLastRow("Drawer_Assign");
+				String sheetName="Drawer_Assign";
+
+
+				for(int row=2;row<=lastrow;row++)
+				{		
+					
+				
+					String RegSSN = TestData.getCellData(sheetName,"SSN",row);	
+						
+						if(SSN.equals(RegSSN))
+						{
+						
+						String PIN = TestData.getCellData(sheetName,"PIN",row);						
+						String CountofDollarCoins = TestData.getCellData(sheetName,"CountofDollarCoins",row);
+						
+
+						
+						test.log(LogStatus.INFO, "Drawer assign process initiated" );
+						 Thread.sleep(4000);
+						 driver.switchTo().defaultContent();
+		                    driver.switchTo().frame("topFrame");
+							wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(text(),'Cash Management')]")));
+
+		                    driver.findElement(By.xpath("//*[contains(text(),'Cash Management')]")).click();
+		                    test.log(LogStatus.PASS, "Clicked on Cash Management");
+		                    Thread.sleep(4000);
+		                 
+		                    driver.switchTo().defaultContent();
+		                    driver.switchTo().frame("mainFrame");
+		                 
+		                    driver.findElement(By.linkText("Drawer")).click();
+		                    test.log(LogStatus.PASS, "Clicked on Drawer");
+		                    Thread.sleep(1000);
+		                    driver.switchTo().defaultContent();
+		                    driver.switchTo().frame("mainFrame");
+		                    driver.findElement(By.linkText("Assign")).click();
+		                    test.log(LogStatus.PASS, "Clicked on Assign");
+		                    Thread.sleep(4000);
+		                    driver.switchTo().defaultContent();
+		                    driver.switchTo().frame("mainFrame");
+		                    driver.switchTo().frame("main");
+
+		                    driver.findElement(By.name("drawerAssignRequestBean.noOf100Dollars")).sendKeys(CountofDollarCoins);
+		                    test.log(LogStatus.PASS,"Count of Dollar Coins is entered as 500");
+		                    Thread.sleep(4000);
+		                    driver.findElement(By.name("drawerAssignRequestBean.password")).sendKeys(PIN);
+		                    test.log(LogStatus.PASS,"Entered the Password as: "+PIN);
+		                    driver.findElement(By.name("drawerassign")).click();
+		                    test.log(LogStatus.PASS,"Clicked on Drawer assign ");
+		                    try {
+		                            Alert alert = driver.switchTo().alert();
+		                            alert.accept();
+		                            test.log(LogStatus.PASS," Alert accepted ");
+
+		                    }
+		                    catch (NoAlertPresentException e) {
+		                        
+
+		                    }
+
+						    		Thread.sleep(4000);
+						    	 test.log(LogStatus.PASS,"Drawer assigned successfully");
+						    	 test.log(LogStatus.PASS, "***********************************************");
+						    	 Thread.sleep(2000);
+						    	 driver.findElement(By.name("done")).click();
+						  
+		                  break; 
+						}
+				}
+		
+				  // break for FOR loop	
+		}
+		
+		catch (Exception e) {
+			test.log(LogStatus.INFO,""+e);
+			test.log(LogStatus.FAIL, "Drawer assign process is initiated again due to Application sync issue");
+
+			e.printStackTrace();
+			
+			
+		}
+		
+		
+	
+}
+}
+		
