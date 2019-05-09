256639a6634f060b1ae189760993daf37e3eda7e
Janaki Devi Guddanti (QFund) <janakidevi.g@qfund.net>
2019-05-09 14:49:09 +0530

    latest update

--- QC_LOC/src/main/java/tests/QCDrawerAssign.java
+++ QC_LOC/src/main/java/tests/QCDrawerAssign.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.relevantcodes.extentreports.LogStatus;
@@ -12,11 +13,11 @@ import com.relevantcodes.extentreports.LogStatus;
 public class QCDrawerAssign extends QCStore{
 	public static void drawerAssign(String SSN,String AppURL) throws Exception
 	{
-		int i;
+		/*int i;
 		for(i=0;i<3;i++)
 		{
 			
-		try{
+		try{*/
 			//String FileName= prop.getProperty("QC_Store_NewLoan_file_name");
 			
 		//	ExcelNew TestData = new ExcelNew(System.getProperty("user.dir")+prop.getProperty("QC_Store_NewLoan_Test_data_sheet_path")+FileName+".xls"); 
@@ -75,58 +76,144 @@ public class QCDrawerAssign extends QCStore{
 		                    test.log(LogStatus.PASS,"Entered the Password as: "+Password);
 		                    driver.findElement(By.name("drawerassign")).click();
 		                    test.log(LogStatus.PASS,"Clicked on Drawer assign ");
-		                    try {
-		                            Alert alert = driver.switchTo().alert();
-		                            alert.accept();
-		                            test.log(LogStatus.PASS," Alert accepted ");
+		                    Thread.sleep(5000);
+		    				//try {
+		    					Alert alert = driver.switchTo().alert();
+		    					String almsg=alert.getText();
+		    					test.log(LogStatus.PASS,"alert with message"+almsg);
+		    					
+		    					if (almsg.contains("Warning")){
+		    						
+		    					alert.accept();
+		    					test.log(LogStatus.PASS,"alert handled" );
+		    					
+		    					Thread.sleep(20000);
+		    					driver.findElement(By.name("done")).click();
+		    					test.log(LogStatus.PASS,"clicked on Ok ");
+		    					test.log(LogStatus.PASS,"Drawer assigned successfully");
+		    					
+		    					
+		    					}
+		    					else
+		    					{
+		    						alert.accept();
+		    						test.log(LogStatus.PASS,"alert handled with messgae safe doesn't contain cash " );
+		    						
+		    						driver.switchTo().defaultContent();
+		    						driver.switchTo().frame("mainFrame");
+		    						driver.findElement(By.linkText("Safe")).click();
+		    						test.log(LogStatus.PASS, "Clicked on Safe");	
+		    						Thread.sleep(3000);
+		    						
+		    						driver.findElement(By.xpath("//*[@id='406']/a")).click();
 
-		                    }
-		                    catch (NoAlertPresentException e) {
-		                        
+		    						test.log(LogStatus.PASS, "Clicked on safe cash-In");
 
-		                    }
-		                   // if(driver.findElement(By.xpath("//input[(@type='button') and (@value='Ok')]")).isDisplayed())
-						    {
-						    		Thread.sleep(10000);
-						    	 test.log(LogStatus.PASS,"Drawer assigned successfully");
-						    	// driver.findElement(By.xpath("//input[(@type='button') and (@value='Ok')]")).click();
-						    	 driver.findElement(By.name("done")).click();
-						    	 //driver.findElement(By.name("done")).click();
-						    }
-						  //  else
-						    {
-						 //   	test.log(LogStatus.PASS,"Drawer not assigned successfully");
-						    }
-						    
-		                  break; 
-						}
-				}
-		
-				break;  // break for FOR loop	
-		}
-		
-		catch (Exception e) {
-			// TODO Auto-generated catch block
-			//test.log(LogStatus.FAIL, MarkupHelper.createLabel("CSR login is failed", ExtentColor.RED));
-			//test.log(LogStatus.FAIL,"Drawer assign is failed");
-			test.log(LogStatus.INFO,"Exception occured "+e.toString().substring(0, 250));
-			String screenshotPath = getScreenhot(driver, "DrawerAssign");
-							test.log(LogStatus.INFO, test.addScreenCapture(screenshotPath));
-			test.log(LogStatus.INFO, "Drawer assign process is initiated again due to Application sync issue");
+		    						Actions action1 = new Actions(driver);
+		    						action1.moveByOffset(200,100).perform();
+		    						Thread.sleep(3000);
+		    						action1.click();
 
-			e.printStackTrace();
-			driver.get(prop.getProperty("login_page"));
-			continue;
-			
-		}
-		
-		
-	}
-		if(i==3)
-		{
-			test.log(LogStatus.FAIL, "Drawer assign is failed");
-	
-		}
-}
-}
-		
+		    						driver.switchTo().defaultContent();
+		    						driver.switchTo().frame("mainFrame");
+		    						driver.switchTo().frame("main");
+		    						
+		    						driver.findElement(By.name("safeCashInRequestBean.noOf100Dollars")).sendKeys("500");
+		    						test.log(LogStatus.PASS,"Count of Dollar Coins is entered as 500");
+		    						
+		    						driver.findElement(By.name("safeCashInRequestBean.password")).sendKeys(Password);
+		    						test.log(LogStatus.PASS,"Entered the Password as: "+Password);
+		    						driver.findElement(By.name("cashIn")).click();
+		    						test.log(LogStatus.PASS,"Clicked on finish");
+		    						
+		    						try { 
+		    							Alert alert1 = driver.switchTo().alert();
+		    							test.log(LogStatus.PASS, "Accepting alert with message"+alert.getText());
+		    							alert1.accept();
+		    							
+		    							//if alert present, accept and move on.														
+
+		    						}
+		    						catch (NoAlertPresentException e) {
+		    							//do what you normally would if you didn't have the alert.
+
+		    						}
+		    						Thread.sleep(2500);
+		    						driver.switchTo().defaultContent();
+		    						driver.switchTo().frame("mainFrame");
+		    						driver.switchTo().frame("main");
+
+		    						if(driver.findElement(By.xpath("//input[(@type='submit') and (@value='Ok')]")).isDisplayed())
+		    						{
+		    							Thread.sleep(2000);
+		    							driver.findElement(By.xpath("//input[(@type='submit') and (@value='Ok')]")).click();
+		    							test.log(LogStatus.PASS,"Clicked on OK button");
+		    							test.log(LogStatus.PASS,"Safe cash in successfully");
+
+		    						}
+		    						else
+		    						{
+		    							test.log(LogStatus.FAIL,"Safe cash -In failed");
+		    						}
+		    						Thread.sleep(2000);
+		    						driver.switchTo().defaultContent();
+		    						driver.switchTo().frame("mainFrame");
+		    				
+		    						driver.findElement(By.linkText("Drawer")).click();
+		    						test.log(LogStatus.PASS, "Clicked on Drawer");
+		    			
+		    						Thread.sleep(500);
+		    						driver.findElement(By.linkText("Assign")).click();
+		    						test.log(LogStatus.PASS, "Clicked on Assign");
+		    						
+		    						Actions action2 = new Actions(driver);
+		    						action2.moveByOffset(200,100).perform();
+		    						Thread.sleep(2000);
+		    						action2.click();
+		    						
+		    						driver.switchTo().defaultContent();
+		    						driver.switchTo().frame("mainFrame");
+		    						driver.switchTo().frame("main");
+
+		    						driver.findElement(By.name("drawerAssignRequestBean.noOf100Dollars")).sendKeys(CountofDollarCoins);
+		    						test.log(LogStatus.PASS,"Count of Dollar Coins is entered as 500");
+		    						Thread.sleep(500);
+		    						driver.findElement(By.name("drawerAssignRequestBean.password")).sendKeys(Password);
+		    						test.log(LogStatus.PASS,"Entered the Password as: "+Password);
+		    						driver.findElement(By.name("drawerassign")).click();
+		    						test.log(LogStatus.PASS,"Clicked on Drawer assign ");
+		    						Thread.sleep(6000);
+		    						try {
+		    							Alert alert5 = driver.switchTo().alert();
+		    							test.log(LogStatus.PASS, "Accepting alert with message"+alert5.getText());
+		    							alert5.accept();
+		    							test.log(LogStatus.PASS," Alert accepted ");
+
+		    						}
+		    						catch (NoAlertPresentException e) {
+		    						}
+		    						Thread.sleep(20000);
+		    						driver.findElement(By.name("done")).click();
+		    						test.log(LogStatus.PASS,"clicked on Ok ");
+		    						test.log(LogStatus.PASS,"Drawer assigned successfully");
+		    						Thread.sleep(1500);
+		    	
+		    					}
+		    					//test.log(LogStatus.PASS," Alert accepted ");
+
+		    				/*}
+		    				catch (NoAlertPresentException e) {
+
+
+		    				}
+		    */				
+		    			}
+
+
+		    			break; 
+		    		}
+		    	}
+
+		    }
+
+
